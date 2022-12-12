1e35b4ee6cdb8c34fb1224f47eb0e798731b04d0
sergioschadenko <sergioschadenko@gmail.com>
2022-12-12 21:30:13 +0300

    new repository

---
+++ Project_0_gues_number/guess-number-game.py
@@ -0,0 +1,65 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+менее чем за 20 попыток
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 1
+    max_number = 101
+    min_number = 1
+    predict_number = np.random.randint(min_number, max_number)
+    
+    if number == predict_number:
+        return count
+    
+    else:
+        while number != predict_number:
+            # Задаем число, если предполгаемое число больше заданного.
+            if number < predict_number:
+                max_number = predict_number
+                predict_number = np.random.randint(min_number, max_number)
+                count += 1
+            # Задаем число, если предполгаемое число меньше заданного.
+            elif number > predict_number:
+                min_number = predict_number
+                predict_number = np.random.randint(min_number, max_number)
+                count += 1
+                
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
